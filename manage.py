052f0768cbd5f7c76297138b8f885bd270e877fa
Leonardo Moraes <demoraesleonardo327@gmail.com>
2025-10-24 22:59:01 -0300

    update

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
-
+import re
 app = Flask(__name__)
 
 
@@ -24,14 +24,19 @@ def start():
 @app.route('/add',methods=['POST'])
 def site():
 
-                nome=request.form.get('nome')
-                numero=request.form.get('numero')
+                nome=request.form.get('nome').strip()
+                numero=request.form.get('numero').strip()
+                if not re.fullmatch(r'[\d\s()+-]+', numero):
+                        return 'Número de telefone inválido!', 400
+               
                 if numero and nome:
+                      
                         novo=Contato(nome=nome,numero=numero)
                         db.session.add(novo)
                         db.session.commit()
                         contatos=Contato.query.order_by(Contato.nome.asc()).all()
                         return  render_template('lista.html',contatos=contatos) 
+
                 else:
                         return 'nome ou numero nao inserido ' ,400
 
